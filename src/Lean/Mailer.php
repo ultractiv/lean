f89567dc10273dded7bcaf16d6bd55687b8b8ce9
Temiyemi Agbetunsin <temi.yemi@gmail.com>
2014-05-16 14:38:35 +0100

    Revised mailer & notification classes

--- src/Lean/Mailer.php
+++ src/Lean/Mailer.php
@@ -7,6 +7,9 @@ class Mailer {
   private $message = array();
   private $mailer;
 
+  protected $defaultSender;
+  protected $defaultRecipient;
+
   protected $service = 'mandrill';
 
   private function __construct() {
@@ -18,25 +21,47 @@ class Mailer {
     	  'important' => false,
     	  'track_opens' => true
     	);
+      $this->setDefaults();
     } catch (\ErrorException $e) {
       Logger::log($e);
     }
+  }
+
+  protected function setDefaults(){
+
+    $this->defaultSender = array(
+      getenv('send_from_email'), getenv('send_from_name')
+    );
+
+    $this->defaultRecipient = array(
+      getenv('send_to_email') => getenv('send_to_name')
+    );
+
   }
 
   public static function instance(){
   	return new self;
   }
 
-  public function setTo(array $recipent) {
-    $this->message['to'] = array();
-    foreach ($recipent as $email=>$name)
+  public function setTo(array $recipient) {
+
+    if ( empty( $recipient ) )
+      $recipient = $this->defaultRecipient;
+
+    foreach ($recipient as $email => $name)
       array_push( $this->message['to'], array( 'email' => $email, 'name' => $name ) );
+
     return $this;
   }
 
   public function setFrom(array $sender) {
+
+    if (empty($sender))
+      $sender = $this->defaultSender;
+
     $this->message['from_email'] = $sender[0];
-    $this->message['from_name'] = $sender[1];
+    $this->message['from_name']  = $sender[1];
+
     return $this;
   }
 
--- src/Lean/Notifier.php
+++ src/Lean/Notifier.php
@@ -4,16 +4,31 @@ namespace Lean;
 
 class Notifier {
 
-  protected  $attachments = null;
   protected  $subject = '';
   protected  $message = '';
-  protected  $recipient;
-  protected  $sender = null;
+  protected  $recipient = array();
+  protected  $sender = array();
+  protected  $attachments = array();
 
+  /*
   protected  $defaultSender;
+  protected  $defaultRecipient;
+  */
 
   public function __construct() {
+
+    /*
+    $this->defaultSender = array(
+      getenv('send_from_email'), getenv('send_from_name')
+    );
+
+    $this->defaultRecipient = array(
+      getenv('send_to_email') => getenv('send_to_name')
+    );
+    */
+
     $this->init();
+
   }
 
   protected function init(){}
@@ -25,8 +40,12 @@ class Notifier {
   // Sends off the notification
   protected function send(){
 
+    /*
     if (!$this->sender)
-      $this->sender = array( $this->defaultSender['email'], $this->defaultSender['name'] );
+      $this->sender = $this->defaultSender;
+
+    if (!$this->recipient)
+      $this->recipient = $this->defaultRecipient;*/
 
     $mailer = Mailer::instance();
     $mailer->setSubject($this->subject)
@@ -37,8 +56,16 @@ class Notifier {
     if (!empty($this->attachments))
       $mailer->setAttachments($this->attachments);
 
+    $this->reset();
+
     $mailer->send();
 
+  }
+
+  protected function reset(){
+    $this->sender      = array();
+    $this->recipient   = array();
+    $this->attachments = array();
   }
   
 }
