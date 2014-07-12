cb84cb7181585f96c810429d69aca0b5624ccb6e
Temiyemi Agbetunsin <temi.yemi@gmail.com>
2014-07-12 01:21:46 +0100

    Refactored app config; now using uppercased names, and global constants

--- src/Lean/Application/Bootstrap.php
+++ src/Lean/Application/Bootstrap.php
@@ -14,6 +14,13 @@ class Bootstrap {
 
   protected $env = 'development';
   protected $config;
+  protected $patterns = array(
+    'ENV_VAR'   => '#^%([a-zA-Z_]+)%$#i',
+    'HTTP_HOST' => '#(localhost|127.0.0.1)#i',
+    'MYSQL_URL' => '#^mysql://(?<user>.+):(?<password>.+)@(?<host>.+)/(?<name>.+)\?(.*)?$#i',
+    'MONGO_URL' => '#^mongodb://(<?host>.+)/(<?name>.+)$#i',
+    'REDIS_URL' => '#^redis://(?<host>.+):(?<port>\d+)$#i'
+  );
 
   public static function instance(){
     return new self;
@@ -28,8 +35,7 @@ class Bootstrap {
     if (!defined('HOST')) define('HOST', "{$_SERVER['HTTP_HOST']}/");
 
     # Test environment
-
-    if (!preg_match('/localhost/i', HOST)) {
+    if (!preg_match($this->patterns['HTTP_HOST'], HOST)) {
       $this->env = 'production';
       # Turn off error reporting for all but the most important errors
       error_reporting(E_ERROR);
@@ -49,54 +55,81 @@ class Bootstrap {
       $config = array_merge_recursive($config['*'], $config[$this->env]);
     }
 
-    putenv('env='. $this->env);
+    putenv('ENVIRONMENT='. $this->env);
 
     $this->config = $config;
 
     if (array_key_exists('database', $config)) {
       $db = $config['database'];
-      putenv('db_host='. $db['host']);
-      putenv('db_user='. $db['user']);
-      putenv('db_pass='. $db['password']);
-      putenv('db_name='. $db['name']);
-      putenv('db_type='. $db['type']);
+      putenv('DATABASE_TYPE='. $db['type']);
+      if (array_key_exists('url', $db) && preg_match($this->patterns['MYSQL_URL'], $this->->read($db['url']), $match)) {
+        $db = $match;
+      }      
+      define('DATABASE_HOST', $this->read($db['host']));
+      define('DATABASE_USER', $this->read($db['user']));
+      define('DATABASE_PASS', $this->read($db['password']));
+      define('DATABASE_NAME', $this->read($db['name']));
     }
 
     if (array_key_exists('memcached', $config)) {
       $mem = $config['memcached'];
-      putenv('memcached_host='. $mem['host']);
-      putenv('memcached_port='. $mem['port']);
+      define('MEMCACHE_SERVERS',  $this->read($mem['servers']));
+      define('MEMCACHE_USERNAME', $this->read($mem['username']));
+      define('MEMCACHE_PASSWORD', $this->read($mem['password']));
     }
 
     if (array_key_exists('mailer', $config)) {
       $mailer = $config['mailer'];
       if (array_key_exists('from', $mailer)) {
-        putenv('send_from_name='  . $mailer['from']['name']);
-        putenv('send_from_email=' . $mailer['from']['email']);
+        putenv('SEND_FROM_NAME='  . $mailer['from']['name']);
+        putenv('SEND_FROM_EMAIL=' . $mailer['from']['email']);
       }
       if (array_key_exists('to', $mailer)) {
-        putenv('send_to_name='  . $mailer['to']['name']);
-        putenv('send_to_email=' . $mailer['to']['email']);
+        putenv('SEND_TO_NAME='  . $mailer['to']['name']);
+        putenv('SEND_TO_EMAIL=' . $mailer['to']['email']);
       }
-      if ($mailer['use']=='mandrill'){
-        putenv('mandrill_api_key=' . $mailer['api_key']);
+      if (strtolower($mailer['use'])=='mandrill'){
+        define('MANDRILL_APIKEY',   $this->read($mailer['api_key']));
+        define('MANDRILL_USERNAME', $this->read($mailer['username']));
+      }
+      if (strtolower($mailer['use'])=='smtp'){
+        define('SMTP_USER',      $this->read($mailer['smtp_user']));
+        define('SMTP_PASSWORD',  $this->read($mailer['smtp_password']));
+        define('SMTP_HOST',      $this->read($mailer['smtp_host']));
+        define('SMTP_PORT',      $this->read($mailer['smtp_port']));
       }
       if (array_key_exists('pretend', $mailer)) {
-        putenv('mailer_pretend=true');
+        putenv('MAILER_PRETEND=true');
       }
     }
 
-    if (array_key_exists('social_api', $config)) {
-      if (array_key_exists('twitter', $config['social'])) {
-        $twitter = $config['social']['twitter'];
-        define('TWITTER_CONSUMER_KEY',    $twitter['consumer_key']);
-        define('TWITTER_CONSUMER_SECRET', $twitter['consumer_secret']);
-        define('TWITTER_ACCESS_TOKEN',    $twitter['access_token']);
-        define('TWITTER_ACCESS_SECRET',   $twitter['access_secret']);
+    if (array_key_exists('webservices', $config)) {      
+      $webservices = $config['webservices'];
+      if (array_key_exists('twitter', $webservices)) {
+        $twitter = $webservices['twitter'];
+        define('TWITTER_CONSUMER_KEY',    $this->read($twitter['consumer_key']));
+        define('TWITTER_CONSUMER_SECRET', $this->read($twitter['consumer_secret']));
+        define('TWITTER_ACCESS_TOKEN',    $this->read($twitter['access_token']));
+        define('TWITTER_ACCESS_SECRET',   $this->read($twitter['access_secret']));
+      }
+      if (array_key_exists('aws', $webservices)) {
+        $aws = $webservices['aws'];
+        define('AWS_CONSUMER_KEY',    $this->read($aws['consumer_key']));
+        define('AWS_CONSUMER_SECRET', $this->read($aws['consumer_secret']));
       }
     }
-
+    
     if (array_key_exists('uploads', $config)) {
+      if (array_key_exists('upload_to', $config)) {
+        if (strtolower($config['upload_to'])=='aws') {
+          # ensure aws configs are set
+          
+        }
+        elseif (strtolower($config['upload_to'])=='filesystem') {
+          
+        }
+      }
+
       $root = realpath(LEAN_APP_ROOT . '/../');
       foreach($config['uploads'] as $resource => $directory)
         putenv("{$resource}_upload_dir=". $root .'/'. $directory);
@@ -107,6 +140,11 @@ class Bootstrap {
   public function application(){
     if (!class_exists('\Router')) throw new RouterException("No Router class found in ".LEAN_APP_ROOT);
     return \Router::instance();
+  }
+
+  private function read($var){
+    if (!preg_match($this->patterns['ENV_VAR'], $var, $match)) return $var;
+    return getenv($match[1]);
   }
 
 } 
