94fba76912dc99d2d29ee1a29606768e9d5805c2
Temiyemi Agbetunsin <temi.yemi@gmail.com>
2014-07-12 01:22:52 +0100

    Add support for multiple Memcached servers

--- src/Lean/Cache.php
+++ src/Lean/Cache.php
@@ -4,14 +4,31 @@ namespace Lean;
 
 class Cache {
 
-  private $cache;
+  private $memcache;
   private $expires = 600; //10 minutes
   
   protected function __construct() {
-    if (class_exists('Memcached')) {
-      $cache = new \Memcached;
-      if ($cache->addServer(getenv('memcached_host'), getenv('memcached_port')))
-        $this->cache = $cache;
+    if (class_exists('\Memcached')) {
+      $m = new \Memcached("memcached_pool");
+      $m->setOption(\Memcached::OPT_BINARY_PROTOCOL, TRUE);
+      // some nicer default options
+      $m->setOption(\Memcached::OPT_NO_BLOCK, TRUE);
+      $m->setOption(\Memcached::OPT_AUTO_EJECT_HOSTS, TRUE);
+      // We use a consistent connection to memcached, so only add in the
+      // servers first time through otherwise we end up duplicating our
+      // connections to the server.
+      if (!$m->getServerList()) {
+        // parse server config
+        $servers = explode(",", \MEMCACHE_SERVERS);
+        foreach ($servers as $s) {
+          $parts = explode(":", $s);
+          $m->addServers($parts[0], $parts[1]);
+        }
+      }
+      // setup authentication
+      if (\MEMCACHE_USERNAME && \MEMCACHE_PASSWORD) 
+        $m->setSaslAuthData( \MEMCACHE_USERNAME, \MEMCACHE_PASSWORD );
+      $this->memcache = $m;      
     }
   }
   
@@ -20,35 +37,35 @@ class Cache {
   }
   
   public function get($key) {
-    if (!$this->cache) return null;
+    if (!$this->memcache) return null;
     
-    $data = $this->cache->get($key);
+    $data = $this->memcache->get($key);
     return false === $data ? null : $data;
   }
   
   public function set($key, $data) {
-    if (!$this->cache) return null;
-    return $this->cache->set($key, $data, $this->expires);
+    if (!$this->memcache) return null;
+    return $this->memcache->set($key, $data, $this->expires);
   }
   
   public function add($key, $data) {
-    if (!$this->cache) return null;
-    return $this->cache->add($key, $data, $this->expires);
+    if (!$this->memcache) return null;
+    return $this->memcache->add($key, $data, $this->expires);
   }
 
   public function delete($key) {
-    if (!$this->cache) return null;
-    return $this->cache->delete($key);
+    if (!$this->memcache) return null;
+    return $this->memcache->delete($key);
   }
   
   public function replace($key, $data){
-    if (!$this->cache) return null;
-    return $this->cache->replace($key, $data, $this->expires);
+    if (!$this->memcache) return null;
+    return $this->memcache->replace($key, $data, $this->expires);
   }
   
   public function flush(){
-    if (!$this->cache) return null;
-    return $this->cache->flush();
+    if (!$this->memcache) return null;
+    return $this->memcache->flush();
   }
   
 }
